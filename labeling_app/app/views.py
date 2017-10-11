e9aea1057cfaa8e890d6d885e7705ab61d83bfb2
Tzahi Vidas <tzahi.vidas@gmail.com>
2017-10-10 20:26:22 -0400

    1. Add images view to flask index page 2. Add crawlers folder for getting images from twitch

--- labeling_app/app/views.py
+++ labeling_app/app/views.py
@@ -1,13 +1,26 @@
 # views.py
 
+import os
 from flask import render_template
+from flask import send_from_directory
 
 from labeling_app.app import app
+from labeling_app import config
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    print("app.root_path", app.root_path)
+    file_path = "live_user_liliasplays-0x7201507137414.jpg"
+    # print(file_path)
+    # file_path = os.path.join(config.MEDIA_FOLDER, "live_user_liliasplays-0x7201507137414.jpg")
+    # print(file_path + " bla\n")
+    return render_template("index.html", file_path=file_path)
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/upload/<filename>')
+def send_unlabeled_image(filename):
+    print(config.UNLABELED_IMAGES_FOLDER, filename, "?????")
+    return send_from_directory(config.UNLABELED_IMAGES_FOLDER, filename)
--- labeling_app/config.py
+++ labeling_app/config.py
@@ -2,3 +2,9 @@
 
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
+
+# Unlabeled images folder
+UNLABELED_IMAGES_FOLDER = "../../unlabeled_images"
+
+# Labeled images folder
+LABELED_IMAGES_FOLDER = "../../labeled_images"
